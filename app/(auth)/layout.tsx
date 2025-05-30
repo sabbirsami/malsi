d4aa8e1b856ad25da854ab6d8f824063559a4840
sabbirsami <smd71430@gmail.com>
2025-05-30 18:20:03 +0600

    initial setups done

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <section>{children}</section>;
+};
+
+export default AuthLayout;
---
+++ app/(root)/layout.tsx
@@ -0,0 +1,5 @@
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return <section>{children}</section>;
+};
+
+export default RootLayout;
